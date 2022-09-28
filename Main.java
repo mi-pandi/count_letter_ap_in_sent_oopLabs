15920743a1e5402524978dc4690d44b15c2259c4
Myroslava Chernyshevych <wchmeri@gmail.com>
2022-09-28 20:07:11 +0300

    Sorting by the letter appearance

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static long charCount(String s, char c) {
+        return s.chars().filter(c1 -> c1 == c).count();
+    }
+
+    public static void main(String[] args) {
+        String text = new Scanner(System.in).nextLine();
+        char letter = new Scanner(System.in).next().charAt(0);
+        //String text = "woorld ooof goooo Hello";
+        //char letter = 'o';
+        String sorted = Arrays.stream(text.split(" "))
+                .sorted(Comparator.comparingLong(w -> charCount(w, letter)))
+                .collect(Collectors.joining(" "));
+        System.out.println(sorted);
+    }
+}
---
+++ out/production/count_letter_ap_in_sent_oopLabs/README.md
@@ -0,0 +1,4 @@
+# count_letter_ap_in_sent_oopLabs
+Counting a letter appearance in the sentence. 
+
+@autor Myroslava Chernyshevych
